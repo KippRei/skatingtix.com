b1a0ddd2aa69724b4d4a1c6b90b67f4221a190f7
kipprei <mkipprei@gmail.com>
2021-05-06 04:18:01 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">      
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A simple platform to sell tickets to any type of skate event!">
+    
+    
+    
+    <!--Testing for Google Search Result Icon-->
+    <link rel="alternate" media="handheld" href="https://skatingtix.com" >
+    
+    
+    
+    
+    
+    <!-- Favicon Icons -->
+    <!-- icon in the highest resolution we need it for -->
+    <link rel="icon" href="icon.png">
+    
+    <!-- reuse same icon for Safari -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    
+    <!-- multiple icons for IE -->
+    <meta name="msapplication-square310x310logo" content="icon_largetile.png">
+
+    <!-- Color for mobile browser window
+    <meta name="theme-color" content="#EE2024"> -->
+    
+    <title>Skatingtix.com</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/scripts/js.js"></script>
+</head>
+
+<body>
+    <div id="banner"></div>
+    <img id="logo" src="/images/logo1.png" alt="Skatingtix.com Logo"/>
+    <img id="bannerText" src="/images/logoText.png" alt="Skatingtix Logo Text"/>
+    <img id="menuBtn" src="/images/menu.png" alt="Menu Button">
+    <hr id="divider" class="brandColor"/>
+
+    <div id="topOfPage"></div>
+    
+    
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
+</body>
+</html>
